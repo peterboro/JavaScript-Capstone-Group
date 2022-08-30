8a8b2e87c4685bc9a9e75b5535b2a5620227ac7f
benjamin kisenge <benkisenge03@gmail.com>
2022-08-30 16:16:06 +0200

    Delete comments

--- src/index.js
+++ src/index.js
@@ -5,9 +5,4 @@ import getMovies from "./modules/getMovies.js";
 
 getMovies(MAIN_URL);
 
-// const getMovies = async () => {
-//   const showMovies = await fetch('https://api.tvmaze.com/shows?page=1');
-//   const data = await showMovies.json();
-//   return data;
-// }
 
