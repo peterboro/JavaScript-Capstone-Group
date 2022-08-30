a818809496a8d77acff74301ecf56ef617155f8f
benjamin kisenge <benkisenge03@gmail.com>
2022-08-30 17:11:31 +0200

    Update UI

--- src/modules/displayMovies.js
+++ src/modules/displayMovies.js
@@ -5,10 +5,14 @@ const showMovies = (data) => {
       const html = `
       <div class="movie">
         <img src='${m.image.medium}' alt="image">
-        <div>
+        <div class="movie-header">
             <h3>${m.name}</h3>
             <i class="fa-solid fa-heart"></i>
-        </div> 
+        </div>
+        <div class="btns"> 
+            <button class="comment">Comment</button>
+            <button class="reservaton">Reservation</button>
+        </div>       
       </div> 
       `;
       movieEl.insertAdjacentHTML('afterbegin',html)
