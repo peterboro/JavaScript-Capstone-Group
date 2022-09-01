744cf4cbf1d6b93dbaf861f1f1eb1b815a860efb
Peter Njuguna <Ptahbn@gmail.com>
2022-09-01 13:32:27 +0300

    Implemented functionality for comment popup and comments counter

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,33 +1,32 @@
-const commentCounter = (data) => (typeof (data) === 'object' ? data.length : 'invalid');
+const getCounter = (data) => (typeof (data) === 'object' ? data.length : 'invalid');
 
-const commentApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8yDe1k19rnGIEx3fLHSF/comments';
-const getMovieComment = async (movieId) => {
-  const response = await fetch(`${commentApi}?item_id=${movieId}`);
+const commentsApiKey = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XAL29eekekrMc3kTORyS/comments';
+const getComment = async (movieId) => {
+  const response = await fetch(`${commentsApiKey}?item_id=${movieId}`);
   return response.json();
 };
 
-const popup = document.querySelector('.comment-popup');
-
-const getTotalComments = async (movieId) => {
-  const result = await getMovieComment(movieId)
-  .then((comment) => (!comment.error ? comment.length : 0))
-  .catch(() => 0);
+const commentPopup = document.querySelector('.comment-popup');
+const getAllComments = async (movieId) => {
+  const result = await getComment(movieId)
+    .then((comment) => (!comment.error ? comment.length : 0))
+    .catch(() => 0);
   return result;
 };
 
-const updateCommentCounter = (movieId) => {
-  getTotalComments(movieId).then((totalComment) => {
-    popup.querySelector('.total-comments').innerHTML = totalComment;
+const updateCounter = (movieId) => {
+  getAllComments(movieId).then((totalComment) => {
+    commentPopup.querySelector('.total-comments').innerHTML = totalComment;
   });
 };
 
-const commentApiEnd = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8yDe1k19rnGIEx3fLHSF/comments';
+const commentApiEndpoint = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XAL29eekekrMc3kTORyS/comments';
 const movieApi = 'https://api.tvmaze.com/shows';
 
 const get = (url) => fetch(url)
-.then((res) => res.json())
-.then((data) => data)
-.catch((error) => error);
+  .then((res) => res.json())
+  .then((data) => data)
+  .catch((error) => error);
 
 const post = (url, params = {}) => fetch(url, {
   method: 'POST',
@@ -42,114 +41,109 @@ const post = (url, params = {}) => fetch(url, {
   .catch((error) => ({ error: true, info: error }));
 
 const addComment = async (params) => {
-  const response = await post(commentApiEnd, params);
+  const response = await post(commentApiEndpoint, params);
   return response;
 };
 
-const getData = async (movieId) => {
+const getMovieData = async (movieId) => {
   const response = await get(`${movieApi}/${movieId}`);
   return response;
 };
 
-const displayMovieComments = (data) => {
-  popup.querySelector('.comments').innerHTML = data;
+const displayComments = (data) => {
+  commentPopup.querySelector('.comments').innerHTML = data;
 };
 
 const showComments = (movieId) => {
-  getMovieComment(movieId).then((data) => {
+  getComment(movieId).then((data) => {
     if (!data.error) {
       let comments = '';
       data.forEach((comment) => {
         comments += `<li class="comments-list">${comment.creation_date} <span> ${comment.username}:</span>  ${comment.comment}</li>`;
       });
-      displayMovieComments(comments);
+      displayComments(comments);
     } else {
-      displayMovieComments('No comment posted yet.');
+      displayComments('No comment posted yet.');
     }
   });
 };
 
 const closePopup = () => {
-  document.querySelector('.close').addEventListener('click', () => {
-    popup.style.display = 'none';
-    popup.innerHTML = '';
+  document.querySelector('#close').addEventListener('click', () => {
+    commentPopup.style.display = 'none';
+    commentPopup.innerHTML = '';
     document.body.style.overflow = 'visible';
   });
 };
 
-
-
-const commentPopup = async (movieId) => {
-  await getData(movieId).then((data) => {
-    popup.innerHTML = `
-    <div class= "popup">
-    <button class="close"><i class="fa fa-times" aria-hidden="true"></i></button>
-    <div class = "container">
-    <div class = "display">
-    <div class = "description">
-    <img src='${data.image}' alt="image">
-    <h3>'${data.name}'</h3>
+const showCommentPopup = async (movieId) => {
+  await getMovieData(movieId).then((data) => {
+    commentPopup.innerHTML = `<div class="popup">
+    <button id="close"><i class="fa fa-times" aria-hidden="true"></i></button>
+    <div class="container">
+        <div class="display">
+          <div class="description">
+            <img src=${data.image.medium} alt="Movie image">
+            <h3 class="movie-title">${data.name}</h3>
+          </div>
+          <div class="display-detail">
+           <ul>
+             <li>${data.summary}</li> 
+             <li><strong>Date of Release:</strong> ${data.premiered}</li>
+             <li><strong>Genres:</strong> ${data.genres}</li>                 
+             <li><strong>Rating:</strong> ${data.rating.average}</li>                 
+           </ul>  
+          </div>
+        </div>
+      <div class="comment-container">
+        <div class="comment-display">
+          <h3 class='counter'>Comments(<span class="total-comments">0</span>)</h3>
+          <ul class="comments">
+          </ul>
+        </div>
+        <div class="comment">
+        <div class="add-comment">
+          <h3>Add Comment</h3>
+        </div>
+        <form class="form">
+          <input type="text" name="name" id="name" placeholder="Provide your Name Please!" required>
+          <textarea name="description" id="description" cols="30" rows="10"
+              placeholder="We'd LOVE to know what you think of the Movie!" required></textarea>
+          <button id=${movieId} type="submit" class="submit-btn">Comment</button>
+        </form>
+      </div>
+      </div>
     </div>
-    <div class="display-detail">
-    <h3> Details </h3>
-    <ul>
-    <li> '${data.summary}' </li>
-    <li><strong>Genres:</strong> ${data.genres}</li>
-    <li><strong>Ratings:</strong> ${data.rating}</li>
-    </ul>
-    </div>
-    </div>
-    <div class = "comment-container">
-    <div class = "comment-display">
-    <h3 class = "counter"> Comments (<span class = "total-comments">0</span>)</h3>
-    <ul class = "comments">
-
-    </ul>
-    </div>
-    <div class = "comment">
-    <div class = "add-comment">
-    <h3>Add Comment</h3>
-    </div>
-    <form class="form">
-    <input type="text" name="name" id="name" placeholder="Your name" required>
-    <textarea name="description" id="description" cols="30" rows="10"
-        placeholder="Your Comment" required></textarea>
-    <button id=${movieId} type="submit" class="submit-btn">Comment</button>
-  </form>
-  </div>
-  </div>
-  </div>
   </div>`;
-  showComments(movieId);
+    showComments(movieId);
 
-  const form = popup.querySelector('.form');
-  form.addEventListener('submit', (e) => {
-    e.prevenDefault();
-    const user = form.elements.name.value;
-    const description = form.elements.description.value;
-    addComment({
-      item_id: movieId,
-      username: user,
-      comment: description,
-    })
-    .then(() => {
-      showComments(movieId);
-      updateCommentCounter(movieId);
-      form.reset();
+    const form = commentPopup.querySelector('.form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const user = form.elements.name.value;
+      const description = form.elements.description.value;
+      addComment({
+        item_id: movieId,
+        username: user,
+        comment: description,
+      }).then(() => {
+        showComments(movieId);
+        updateCounter(movieId);
+        form.reset();
+      });
     });
   });
-  });
-  popup.style.display = 'block';
+  commentPopup.style.display = 'block';
   closePopup();
 };
 
 document.addEventListener('click', async (e) => {
   if (e.target.matches('.comment')) {
-    commentPopup(e.target.id);
-    const comment = await getMovieComment(e.target.id);
-    commentCounter(comment);
-    updateCommentCounter(e.target.id);
+    showCommentPopup(e.target.id);
+    const comment = await getComment(e.target.id);
+    getCounter(comment);
+    updateCounter(e.target.id);
   }
 });
 
-export {commentPopup, commentCounter};
+export { showCommentPopup, getCounter };
