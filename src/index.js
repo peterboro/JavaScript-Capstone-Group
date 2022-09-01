7d0edf7461ab777a7f2e63edb0922055702dfcac
benjamin kisenge <benkisenge03@gmail.com>
2022-09-01 15:05:15 +0200

    Create like module

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ const MAIN_URL = 'https://api.tvmaze.com/shows';
 getMovies(MAIN_URL);
 
 document.addEventListener('click', (e) => {
-  if (e.target.matches('.comment')) {
+  if (e.target.matches('.comments')) {
     showCommentPopup(e.target.id);
+  }
+  if (e.target.matches('.fa-heart')) {
+    console.log('you liked me');
   }
 });
--- src/modules/displayMovies.js
+++ src/modules/displayMovies.js
@@ -6,10 +6,13 @@ const showMovies = (data) => {
         <img src='${m.image.medium}' alt="image">
         <div class="movie-header">
             <h3>${m.name}</h3>
-            <i class="fa-solid fa-heart"></i>
+            <div class="liked">
+                <i class="fa-solid fa-heart"></i>
+                <span>${1} likes</span>
+            </div>
         </div>
         <div class="btns"> 
-            <button id="${m.id}" class="comment">Comment</button>
+            <button id="${m.id}" class="comments">Comment</button>
             <button class="reservaton">Reservation</button>
         </div>       
       </div> 
