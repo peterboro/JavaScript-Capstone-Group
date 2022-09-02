7f38ed3ad49b95ceaa037875369aee4b6c3d6a59
benjamin kisenge <benkisenge03@gmail.com>
2022-09-02 12:29:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,9 @@ import getMovies from './modules/getMovies.js';
 const MAIN_URL = 'https://api.tvmaze.com/shows';
 
 getMovies(MAIN_URL);
-
-// const items = document.querySelector('.movie-info');
-
-// movieNbr.innerHTML = items.children.length; 
-// console.log(items.children)
+ 
 document.addEventListener('click', (e) => { 
   if (e.target.matches('.comments')) {
     showCommentPopup(e.target.id);
-  }
-  if (e.target.matches('.fa-heart')) {
-    
-    // console.log('movie', items.length )
-  }
+  } 
 });
